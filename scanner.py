22edb39265204a345bcb3fdf832a1d85a5e4726d
ProgSaif <isaifulone@gmail.com>
2026-03-11 14:02:02 +0600

    Create scanner.py

---
+++ scanner.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import requests
+
+def get_klines(symbol, interval="1h", limit=200):
+    url = f"https://api.binance.com/api/v3/klines?symbol={symbol}&interval={interval}&limit={limit}"
+    data = requests.get(url).json()
+    df = pd.DataFrame(data, columns=[
+        "open_time","open","high","low","close","volume",
+        "close_time","quote_asset_volume","trades",
+        "taker_buy_base","taker_buy_quote","ignore"
+    ])
+    df = df[["open","high","low","close","volume"]].astype(float)
+    return df
